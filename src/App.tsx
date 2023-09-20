9cf6eccd4f79aab1f99ad2e0bdd05c9f2913c796
kristian diez <isnomatophobic@gmail.com>
2023-09-20 19:40:52 +0300

    Async read from csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { ChangeEvent } from "react";
 import "./App.css";
+import { parse } from "papaparse";
 
+type Data = {
+  EmpID: string;
+  ProjectID: string;
+  DateFrom: string;
+  DateTo: string;
+};
 function App() {
-  return <>Hello world</>;
+  const changeHandler = async (event: ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    const dataArray = await new Promise<Data[]>((resolve, reject) => {
+      if (!target.files) return [];
+      parse(target.files[0], {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results) => {
+          resolve(results.data as Data[]);
+        },
+        error: () => {
+          reject();
+        },
+      });
+    });
+    console.log(dataArray);
+  };
+  return (
+    <div>
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={changeHandler}
+        style={{ display: "block", margin: "10px auto" }}
+      />
+    </div>
+  );
 }
 
 export default App;
